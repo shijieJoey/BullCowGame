82bfe628af3a3bf98dc78a9e7ed0920374d05227
Shijie Xiong <joeyxiong1994@gmail.com>
2021-03-05 23:37:45 -0800

    Modifying basic game logic

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,46 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+
+    SetupGame();
+
+    // Debug what the hidden word is
+    // PrintLine(TEXT("The HiddenWord is: %s."), *HiddenWord);
+
+    PrintLine(TEXT("Welcome to the Bull Cows!"));
+    PrintLine(TEXT("Guess the %i letter word!"), HiddenWord.Len());
+    PrintLine(TEXT("Type in your guess and press enter to continue..."));    
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
+    ClearScreen();
 
+
+
+
+
+
+    if (Input == HiddenWord)
+    {
+        PrintLine(TEXT("You have won!"));
+    }
+    else 
+    {
+        if(Input.Len() != HiddenWord.Len())
+        {
+            PrintLine(TEXT("The Hidden Word is %i characters long, try again!"), HiddenWord.Len());
+        }
+        
+        PrintLine(TEXT("You have lost!"));
+    }
+    
+    
+}
+
+void UBullCowCartridge::SetupGame() 
+{
+    HiddenWord = TEXT("cake");
+    Lives = 4;
+    bGameOver = false;
 }
